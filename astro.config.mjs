52b3d24d1caa05f3d1cf2f207f9016f66217935a
Kuba808 <jakub.sleis@gmail.com>
2026-04-13 20:13:37 +0200

    fix: remove base path update site to spolecnebroumov.cz

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://kuba808.github.io',
-    base: '/spolecne/',
+    site: 'https://www.spolecnebroumov.cz',
 });
